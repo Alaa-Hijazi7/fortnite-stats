fc0a742c84616871876868e14770ad4425556aa3
Alaa-Hijazi7 <alaa.hijayyad@gmail.com>
2021-02-28 17:22:56 +0200

    setup html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import logo from './assets/images/logo.png';
+import github from "./assets/images/github-square-brands.svg";
+import instagramLogo from "./assets/images/instagramLogo.png";
 import './App.css';
+import './assets/style/header.css';
+import './assets/style/userinfo.css';
+import './assets/style/card.css';
+
+function Card() {
+  return (
+    <div className="card">
+      <h2>12323</h2>
+      <h3>Kills</h3>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="App">
+      <header>
+        <nav>
+          <img src={logo} alt="logo" id="logo" />
+          <div>
+            <a href="https://www.instagram.com/alaahijazi.7/" target="_blank" rel="noreferrer"><img src={instagramLogo} alt="instagram logo" id="instagram-logo" /></a>
+            <a href="https://github.com/Alaa-Hijazi7" target="_blank" rel="noreferrer"><img src={github} alt="github logo" id="github-logo" /></a>
+          </div>
+        </nav>
+        <input type="text" placeholder="search with username" />
       </header>
-    </div>
+      <section id="user-info">
+        <img src="https://i.pinimg.com/564x/98/a8/a3/98a8a3155d35787032a5105084e95832.jpg" alt="user avatar" />
+        <div>
+          <h3 id="username">Alaa</h3>
+          <h4 id="platform">platform: <span>PC</span></h4>
+        </div>
+      </section>
+      <section id="cards">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </section>
+    </main>
   );
 }
 
