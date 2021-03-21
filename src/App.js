93017265ef8b0a4dfe2a6e034096c2b71ce007a5
Alaa-Hijazi7 <alaa.hijayyad@gmail.com>
2021-03-21 10:24:49 +0200

    eslint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { useState, useReducer } from "react";
 import axios from "axios";
-import logo from './assets/images/logo.png';
-import github from "./assets/images/github-square-brands.svg";
-import instagramLogo from "./assets/images/instagramLogo.png";
-import './assets/style/header.css';
-import './assets/style/userinfo.css';
-import './assets/style/card.css';
-import './App.css';
+import "./assets/style/header.css";
+import "./assets/style/userinfo.css";
+import "./assets/style/card.css";
+import "./App.css";
 
 const initState = {
   data: {},
@@ -38,40 +35,34 @@ function reducer(state, action) {
       return state;
   }
 }
+
 function Card(props) {
   return (
     <div className="card">
-      <h2>{ props.count }</h2>
-      <h3>{ props.name }</h3>
+      <h2>{props.count}</h2>
+      <h3>{props.name}</h3>
     </div>
   )
 }
 
-function App() {
+export default function App() {
   const [name, setName] = useState("");
   const [state, dispatch] = useReducer(reducer, initState);
 
   async function handleClick(e) {
     e.preventDefault();
     try {
-      const action = { type: "loading" };
-      dispatch(action);
-      const { data } = await axios.get(
-        `https://fortnite-api.p.rapidapi.com/stats/${name}`,
-        {
-          headers: {
-            "x-rapidapi-host": "fortnite-api.p.rapidapi.com",
-            "x-rapidapi-key":
-              "5a0bae066cmsh936eabf8958a7d4p10e690jsn3c51ecb2bce6",
-          },
-        }
-      );
-      const action2 = { type: "success", payload: data };
-      dispatch(action2);
+      dispatch({ type: "loading" });
+      const { data } = await axios.get(`https://fortnite-api.p.rapidapi.com/stats/${name}`, {
+        headers: {
+          "x-rapidapi-host": "fortnite-api.p.rapidapi.com",
+          "x-rapidapi-key": "5a0bae066cmsh936eabf8958a7d4p10e690jsn3c51ecb2bce6",
+        },
+      });
+      dispatch({ type: "success", payload: data });
     } catch (error) {
-      let errMsg = "hi";
-      const action3 = { type: "error", error: errMsg };
-      dispatch(action3);
+      let errMsg = "We found an error here 😥";
+      dispatch({ type: "error", error: errMsg });
     }
   }
 
@@ -79,10 +70,14 @@ function App() {
     <main className="App">
       <header>
         <nav>
-          <img src={logo} alt="logo" id="logo" />
+          <img src="./assets/images/logo.png" alt="logo" id="logo" />
           <div>
-            <a href="https://www.instagram.com/alaahijazi.7/" target="_blank" rel="noreferrer"><img src={instagramLogo} alt="instagram logo" id="instagram-logo" /></a>
-            <a href="https://github.com/Alaa-Hijazi7" target="_blank" rel="noreferrer"><img src={github} alt="github logo" id="github-logo" /></a>
+            <a href="https://www.instagram.com/alaahijazi.7/" target="_blank" rel="noreferrer">
+              <img src="./assets/images/instagramLogo.png" alt="instagram logo" id="instagram-logo" />
+            </a>
+            <a href="https://github.com/Alaa-Hijazi7" target="_blank" rel="noreferrer">
+              <img src="./assets/images/github-square-brands.svg" alt="github logo" id="github-logo" />
+            </a>
           </div>
         </nav>
         <form onSubmit={handleClick}>
@@ -95,8 +90,8 @@ function App() {
         <section id="user-info">
           <img src="https://i.pinimg.com/564x/98/a8/a3/98a8a3155d35787032a5105084e95832.jpg" alt="user avatar" />
           <div>
-            <h3 id="username">displayName: <span>{state.data.user?.displayName}</span></h3>
-            <h4 id="platform">platform: <span>{ state.data.user?.externalAuths?.psn?.type || "PC" }</span></h4>
+            <h3 id="username">Display name: <span>{state.data.user?.displayName}</span></h3>
+            <h4 id="platform">Platform: <span>{ state.data.user?.externalAuths?.psn?.type || "PC" }</span></h4>
           </div>
         </section>
         <section id="cards">
@@ -108,11 +103,8 @@ function App() {
           <Card name="Place Top 10" count={state.data.season.all.all.placetop10 === 0 ? "0" : state.data.season.all.all.placetop10 || "not sure"} />
           <Card name="Place Top 25" count={state.data.season.all.all.placetop25 === 0 ? "0" : state.data.season.all.all.placetop25 || "not sure"} />
         </section>
-      </React.Fragment>
-      }
-      {<h1 id="errorMsg">{state.data?.error}</h1> }
+      </React.Fragment>}
+      {<h1 id="errorMsg">{state.data?.error}</h1>}
     </main>
   );
 }
-
-export default App;
